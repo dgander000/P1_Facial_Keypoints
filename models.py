cbcbf3c5adc4689bd595be89abfa06998d38e64d
Dan Gander <dgander000@gmail.com>
2021-01-04 04:50:39 -0600

    First pass at defining model

--- models.py
+++ models.py
@@ -17,14 +17,38 @@ class Net(nn.Module):
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        # Inputs size: 224 x 224
+        # Filter: 5 x 5 x 32
+        # Output size: (W-F)/S + 1 = (224-5)/1 + 1 = 220
+        # Output tensor: (32, 220, 220)
         self.conv1 = nn.Conv2d(1, 32, 5)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+       
+        # pool with kernel_size=2, stride=2
+        # Output tensor: (32, 110, 110)
+        self.pool = nn.MaxPool2d(2, 2)
         
+        # second conv layer: 32 inputs, 64 outputs, 5x5 conv
+        ## output size = (W-F)/S + 1 = (110-5)/1 + 1 = 106
+        # the output tensor will have dimensions: (64, 106, 106)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        
+        # pool with kernel_size=2, stride=2
+        # Output tensor: (64, 53, 53)
+
+        # 64 outputs * the 5*5 filtered/pooled map size
+        self.fc1 = nn.Linear(64*53*53, 256)
+        
+        # dropout with p=0.4
+        self.fc1_drop = nn.Dropout(p=0.4)
+        
+        # finally, create 68*2 output channels (for the 68 keypoints)
+        self.fc2 = nn.Linear(256, 68*2)
 
         
     def forward(self, x):
@@ -32,6 +56,17 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+
+        # prep for linear layer
+        # this line of code is the equivalent of Flatten in Keras
+        x = x.view(x.size(0), -1)
         
-        # a modified x, having gone through all the layers of your model, should be returned
+        # two linear layers with dropout in between
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
+        
+        # final output
         return x
