25302ac6177acf63d496e576eb812f0fdea6959c
Dan Gander <dgander000@gmail.com>
2021-01-08 07:56:51 -0600

    Model updates

--- models.py
+++ models.py
@@ -18,39 +18,30 @@ class Net(nn.Module):
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
-
         # Input size: 224 x 224
+        self.conv1 = nn.Conv2d(1, 32, 7, 1) # output size = (W-F)/S + 1 = (224-7)/1 + 1 = 218 -- (32, 218, 218)
+        self.pool1 = nn.MaxPool2d(2)        # (32, 109, 109)
+        self.drop1 = nn.Dropout(p = 0.1)
 
-        self.conv1 = nn.Conv2d(1, 32, 4, 1) # output size = (W-F)/S + 1 = (224-4)/1 + 1 = 221 -- (32, 221, 221)
-        self.actv1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(2)        # (32, 110, 110)
-        self.drop1 = nn.Dropout(0.1)
+        self.conv2 = nn.Conv2d(32, 64, 5, 1) # output size = (W-F)/S + 1 = (109-5)/1 + 1 = 105 -- (64, 105, 105)
+        self.pool2 = nn.MaxPool2d(2)         # (64, 52, 52)
+        self.drop2 = nn.Dropout(p = 0.2)
+        
+        self.conv3 = nn.Conv2d(64, 128, 5, 1) # output size = (W-F)/S + 1 = (52-5)/1 + 1 = 48 -- (128, 48, 48)
+        self.pool3 = nn.MaxPool2d(2)          # (128, 24, 24)
+        self.drop3 = nn.Dropout(p = 0.2)
 
-        self.conv2 = nn.Conv2d(32, 64, 3, 1) # output size = (W-F)/S + 1 = (110-3)/1 + 1 = 108 -- (64, 108, 108)
-        self.actv2 = nn.ReLU() 
-        self.pool2 = nn.MaxPool2d(2)         # (64, 54, 54)
-        self.drop2 = nn.Dropout(0.2)
+        self.conv4 = nn.Conv2d(128, 256, 3, 1) # output size = (W-F)/S + 1 = (24-3)/1 + 1 = 22 -- (256, 22, 22)
+        self.pool4 = nn.MaxPool2d(2)          # (256, 11, 11)
+        self.drop4 = nn.Dropout(p = 0.2)
 
-        self.conv3 = nn.Conv2d(64, 128, 2, 1) # output size = (W-F)/S + 1 = (54-2)/1 + 1 = 53 -- (128, 53, 53)
-        self.actv3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(2)          # (128, 26, 26)
-        self.drop3 = nn.Dropout(0.3) 
+        self.conv5 = nn.Conv2d(256, 512, 1, 1) # output size = (W-F)/S + 1 = (11-1)/1 + 1 = 11 -- (256, 11, 11)
+        self.pool5 = nn.MaxPool2d(2)          # (512, 5, 5)
+        self.drop5 = nn.Dropout(p = 0.3)
 
-        self.conv4 = nn.Conv2d(128, 256, 1, 1) # output size = (W-F)/S + 1 = (26-1)/1 + 1 = 26 -- (256, 26, 26)
-        self.actv4 = nn.ReLU()
-        self.pool4 = nn.MaxPool2d(2)          # (256, 13, 13)
-        self.drop4 = nn.Dropout(0.4)
-
-        self.fc1 = nn.Linear(256*13*13, 1024)
-        self.actv5 = nn.ReLU()
-        self.drop5 = nn.Dropout(0.5)
-
-        self.fc2 = nn.Linear(1024, 1024)
-        self.actv6 = nn.ReLU()
-        self.drop6 = nn.Dropout(0.6)
-
-        # finally, create 68*2 output channels (for the 68 keypoints)
-        self.fc3 = nn.Linear(1024, 68*2)
+        self.fc1 = nn.Linear(512*5*5, 1024)
+        self.drop6 = nn.Dropout(p = 0.4)
+        self.fc2 = nn.Linear(1024, 68*2)
 
         
     def forward(self, x):
@@ -60,17 +51,22 @@ class Net(nn.Module):
         
         
         # a modified x, having gone through all the layers of your model, should be returned
-        x = self.drop1(self.pool1(self.actv1(self.conv1(x))))
-        x = self.drop2(self.pool2(self.actv2(self.conv2(x))))
-        x = self.drop3(self.pool3(self.actv3(self.conv3(x))))
-        x = self.drop4(self.pool4(self.actv4(self.conv4(x))))
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.drop1(x)
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.drop2(x)
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        x = self.pool4(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        x = self.pool5(F.relu(self.conv5(x)))
+        x = self.drop5(x)
 
         # prep for linear layer
         # this line of code is the equivalent of Flatten in Keras
         x = x.view(x.size(0), -1)
-        x = self.drop5(self.actv5(self.fc1(x)))
-        x = self.drop6(self.actv6(self.fc2(x))) 
-        x = self.fc3(x)
-        
+        x = F.relu(self.fc1(x))
+        x = self.drop6(x)
+        x = self.fc2(x)
         # final output
         return x
